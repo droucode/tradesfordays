92bd7b0678c36b294eba7bab6684e9dfdd7d4896
droucode <iamdrou@gmail.com>
2021-12-15 09:57:53 +1100

    tradeservice model sanitising, after create/update

--- app/models/tradeservice.rb
+++ app/models/tradeservice.rb
@@ -8,4 +8,20 @@ class Tradeservice < ApplicationRecord
     validates :description, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
     validates :status, presence: true
     validates :location, presence: true
+
+    after_create :log_create
+    after_update :log_update
+
+    def to_s
+        "Tradeservuce #{id}; #{name}, #{budget},#{trade}, #{description}, #{status}, #{location}"
+    end
+
+    def log_create
+        logs.create! body: 'Created'
+    end
+
+    def log_update
+        logs.create! body: 'Updated'
+    end
+
 end
