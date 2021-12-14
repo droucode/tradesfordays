aef38c2f83d771d85aa398b7d34b70549cbde143
droucode <iamdrou@gmail.com>
2021-12-15 10:33:46 +1100

    tradeservice model sanitising, after validation- final

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,7 +2,7 @@ class Enquiry < ApplicationRecord
   belongs_to :user
   belongs_to :tradeservice
   belongs_to :quote
-  validates :task, presence: true
+  validates :task, presence: true, length: { maximum: 250 }
   validates :date, presence: true
   validates :address, presence: true 
   validates :user_id, presence: true
--- app/models/tradeservice.rb
+++ app/models/tradeservice.rb
@@ -2,12 +2,12 @@ class Tradeservice < ApplicationRecord
     has_many :enquiries
     has_many :quotes 
     has_one_attached :picture
-    validates :name, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
+    validates :name, presence: true, length: {minimum: 2}
     validates :budget, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
-    validates :trade, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
-    validates :description, presence: true
-    validates :status, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
-    validates :location, presence: true, format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/ }
+    validates :trade, presence: true
+    validates :description, presence: true, length: { maximum: 250 }
+    validates :status, presence: true
+    validates :location, presence: true
 
     after_create :log_create
     after_update :log_update
