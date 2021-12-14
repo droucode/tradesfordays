44b25de910b996e398b92346d8413d527fb93d56
droucode <iamdrou@gmail.com>
2021-12-15 08:23:00 +1100

    before_save and before_validation added to user

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,7 +2,7 @@ class Enquiry < ApplicationRecord
   belongs_to :user
   belongs_to :tradeservice
   belongs_to :quote
-  validates :task, presence: true 
+  validates :task, presence: true,  format: { with: /\A[a-zA-Z][a-zA-Z ]+\z/, message: "alphabets only" }
   validates :date, presence: true
   validates :address, presence: true 
   validates :user_id, presence: true
